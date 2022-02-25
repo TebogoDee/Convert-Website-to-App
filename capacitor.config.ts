7848006e7d941ded7043b7c9858b7e8d637d403f
Tee_Cee <40995805+TebogoDee@users.noreply.github.com>
2022-02-25 05:06:31 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'co.za.weistream.WeistreaM',
+  appName: 'WeistreaM',
   webDir: 'www',
   bundledWebRuntime: false,
 };
